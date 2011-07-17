0203950a5954e940443a232e3ca391163f0f4055
Spike Grobstein <spikegrobstein@mac.com>
2011-07-16 22:54:51 -0400

    added support for actually launching boxee when we get a discover packet.

--- boxee_launcher.js
+++ boxee_launcher.js
@@ -1,13 +1,30 @@
-var dgram = require("dgram");
+// configuration:
 
+var path_to_boxee = "/Applications/Boxee.app";
+
+// udp socket stuff
+var dgram = require("dgram");
 var server = dgram.createSocket("udp4");
+
+// shell execution stuff
+var util = require('util'),
+    exec = require('child_process').exec,
+    child;
 
 server.on("message", function (msg, rinfo) {
   //console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
 
 	// if it receives a discover packet, then let's try to launch boxee
 	if ( msg.toString('utf8').match(/cmd="discover"/) ) {
-		console.log("We got a discover packet!!!");
+		console.log("We got a discover packet!!! Launching Boxee at " + path_to_boxee);
+		
+		child = exec('open "' + path_to_boxee + '"',
+		  function (error, stdout, stderr) {
+		    if (error !== null) {
+		      console.log('exec error: ' + error);
+		    }
+		});
+		
 	} else {
 		console.log("NOT a discover packet...");
 	}
