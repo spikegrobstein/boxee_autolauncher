7a46969f4e5781fb649ed8149fcf478649881dcd
Spike Grobstein <spikegrobstein@mac.com>
2011-07-17 00:54:31 -0400

    changed to util.log instead of console.log()

--- boxee_autolauncher.js
+++ boxee_autolauncher.js
@@ -15,12 +15,12 @@ var util = require('util'),
 
 // if boxee isn't running, listen, otherwise, wait until boxee stops running, then listen.
 function listen () {
-	console.log('ready to listen... check if boxee is running...');
+	util.log('ready to listen... check if boxee is running...');
 	
 	try {
 		server.bind(2562);
 	} catch (err) {
-		console.log("Boxee appears to be running! waiting 2.5 seconds.");
+		util.log("Boxee appears to be running! waiting 2.5 seconds.");
 		setTimeout(listen, 2500); // wait 2.5 seconds and try again (in case boxee exits)
 	}
 
@@ -30,24 +30,24 @@ function listen () {
 // checks this by looking at child.pid and hitting it with kill -0
 function is_boxee_running (callback_on_true, callback_on_false) {
 	if (typeof callback_on_true !== 'function') {
-		console.log("callback_on_true not defined.");
+		util.log("callback_on_true not defined.");
 		callback_on_true = function() { return false; };
 	}
 	
 	if (typeof callback_on_false !== 'function') {
-		console.log("callback_on_false not defined.");
+		util.log("callback_on_false not defined.");
 		callback_on_false = function() { return false; };
 	}
 	
 	if (!child) {
-		console.log("no child process... must not be running.")
+		util.log("no child process... must not be running.")
 		// if there's no child, yet, then it's not running
 		callback_on_false();
 		return;
 	}
 	
 	// send kill -0 to child.pid.
-	console.log("Running Kill command");
+	util.log("Running Kill command");
 	var kill_cmd = exec('kill -0 ' + child.pid,
 		function (error, stdout, stderr) {
 			(error !== null) ? callback_on_true() : callback_on_false();
@@ -55,17 +55,17 @@ function is_boxee_running (callback_on_true, callback_on_false) {
 }
 
 server.on("message", function (msg, rinfo) {
-  //console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+  //util.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
 
 	// if it receives a discover packet, then let's try to launch boxee
 	if ( msg.toString('utf8').match(/cmd="discover"/) ) {
-		console.log("We got a discover packet!!! Launching Boxee at " + path_to_boxee);
+		util.log("We got a discover packet!!! Launching Boxee at " + path_to_boxee);
 		
 		// launch boxee!
 		child = exec('open "' + path_to_boxee + '"',
 		  function (error, stdout, stderr) {
 		    if (error !== null) {
-		      console.log('exec error: ' + error);
+		      util.log('exec error: ' + error);
 		    }
 		});
 		
@@ -73,14 +73,14 @@ server.on("message", function (msg, rinfo) {
 		listen();
 		
 	} else {
-		console.log("NOT a discover packet...");
+		util.log("NOT a discover packet...");
 	}
 	
 });
 
 server.on("listening", function () {
   var address = server.address();
-  console.log("server listening " + address.address + ":" + address.port);
+  util.log("server listening " + address.address + ":" + address.port);
 });
 
 listen();
