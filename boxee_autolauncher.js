04323a1f92472bbe35de433328d7af79761e5e6f
Spike Grobstein <spikegrobstein@mac.com>
2011-07-16 23:45:15 -0400

    rewrote it a bit to support killing and checking if things are running. added console log output for everything.

--- boxee_autolauncher.js
+++ boxee_autolauncher.js
@@ -2,7 +2,7 @@
 
 // configuration:
 
-var path_to_boxee = "/Applications/Boxee.app";
+var path_to_boxee = "/Applications/TextEdit.app";
 
 // udp socket stuff
 var dgram = require("dgram");
@@ -12,6 +12,66 @@ var server = dgram.createSocket("udp4");
 var util = require('util'),
     exec = require('child_process').exec,
     child;
+
+// if boxee isn't running, listen, otherwise, wait until boxee stops running, then listen.
+function listen () {
+	console.log('ready to listen... check if boxee is running...');
+	
+	is_boxee_running(
+		function() {
+			console.log("Boxee is running... let's wait 2.5 seconds and try again...");
+
+			setTimeout(listen, 2500); // wait 2.5 seconds and try to listen again
+		},
+		function() {
+			console.log("Boxee is not running... let's start listening.");
+			
+			try {
+				server.bind(2562);
+			} catch (err) {
+				console.log("Boxee MUST be running!");
+				child = true;
+			}
+			
+			setTimeout(listen, 2500); // wait 2.5 seconds and try again (in case boxee exits)
+		}
+	);
+
+}
+
+// returns true if boxee is running
+// checks this by looking at child.pid and hitting it with kill -0
+function is_boxee_running (callback_on_true, callback_on_false) {
+	if (typeof callback_on_true !== 'function') {
+		console.log("callback_on_true not defined.");
+		callback_on_true = function() { return false; };
+	}
+	
+	if (typeof callback_on_false !== 'function') {
+		console.log("callback_on_false not defined.");
+		callback_on_false = function() { return false; };
+	}
+	
+	if (!child) {
+		console.log("no child process... must not be running.")
+		// if there's no child, yet, then it's not running
+		callback_on_false();
+		return;
+	}
+	
+	// this happens when we try to listen on the UDP port, but boxee is already running and listening.
+	if (child === true) {
+		callback_on_true();
+		return;
+	}
+	
+	// send kill -0 to child.pid.
+	console.log("Running Kill command");
+	var kill_cmd = exec('kill -0 ' + child.pid,
+		function (error, stdout, stderr) {
+			(error !== null) ? callback_on_true() : callback_on_false();
+	});
+}
 
 server.on("message", function (msg, rinfo) {
   //console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
@@ -28,6 +88,8 @@ server.on("message", function (msg, rinfo) {
 		    }
 		});
 		
+		server.close();
+		
 	} else {
 		console.log("NOT a discover packet...");
 	}
@@ -39,4 +101,4 @@ server.on("listening", function () {
   console.log("server listening " + address.address + ":" + address.port);
 });
 
-server.bind(2562);
+listen();
